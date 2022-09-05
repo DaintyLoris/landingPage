920ca75bf2dd93db509836e837063e2dbebd270e
josue <josue.anguiano6869@alumnos.udg.mx>
2022-09-05 17:02:24 -0500

    creacion de proyecto

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <!-- Favicons -->
+        <link href="/img/favicon.png" rel="icon">
+        <link href="/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="/vendor/aos/aos.css" rel="stylesheet">
+        <link href="/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="/css/main.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-header">
+            <h2>Contact</h2>
+            <p>Datos personales para hacer contacto</p>
+            </div>
+
+            <div class="row gx-lg-0 gy-4">
+
+            <div class="col-lg-4">
+
+                <div class="info-container d-flex flex-column align-items-center justify-content-center">
+                <div class="info-item d-flex">
+                    <i class="bi bi-geo-alt flex-shrink-0"></i>
+                    <div>
+                    <h4>Location:</h4>
+                    <p>A108 Adam Street, New York, NY 535022</p>
+                    </div>
+                </div><!-- End Info Item -->
+
+                <div class="info-item d-flex">
+                    <i class="bi bi-envelope flex-shrink-0"></i>
+                    <div>
+                    <h4>Email:</h4>
+                    <p>info@example.com</p>
+                    </div>
+                </div><!-- End Info Item -->
+
+                <div class="info-item d-flex">
+                    <i class="bi bi-phone flex-shrink-0"></i>
+                    <div>
+                    <h4>Call:</h4>
+                    <p>+1 5589 55488 55</p>
+                    </div>
+                </div><!-- End Info Item -->
+
+                <div class="info-item d-flex">
+                    <i class="bi bi-clock flex-shrink-0"></i>
+                    <div>
+                    <h4>Open Hours:</h4>
+                    <p>Mon-Sat: 11AM - 23PM</p>
+                    </div>
+                </div><!-- End Info Item -->
+                </div>
+
+            </div>
+
+            <div class="col-lg-8">
+                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="row">
+                    <div class="col-md-6 form-group">
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Nombre" required>
+                    </div>
+                    <div class="col-md-6 form-group mt-3 mt-md-0">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Correo" required>
+                    </div>
+                </div>
+                <div class="form-group mt-3">
+                    <input type="text" class="form-control" name="asunto" id="asunto" placeholder="Asunto" required>
+                </div>
+                <div class="form-group mt-3">
+                    <textarea class="form-control" name="comentario" rows="7" placeholder="comentario" required></textarea>
+                </div>
+                <div class="my-3">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Enviar</button></div>
+                </form>
+            </div><!-- End Contact Form -->
+
+            </div>
+
+        </div>
+        </section><!-- End Contact Section -->
+        
+        <!-- Vendor JS Files -->
+        <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="/vendor/aos/aos.js"></script>
+        <script src="/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="/vendor/php-email-form/validate.js"></script>
+
+        <!-- Template Main JS File -->
+        <script src="/js/main.js"></script>
+
+    </body>
+
+</html>
